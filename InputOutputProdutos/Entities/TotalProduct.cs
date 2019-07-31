9b29ce1854afadc6106ad1638babbced42dffc15
Rodrigo Freitas <rodrigo.santos203@fatec.sp.gov.br>
2019-07-30 21:49:25 -0300

    Criacao do programa de leitura, criacao de pastas e gravacao de arquivos

---
+++ InputOutputProdutos/Entities/TotalProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace InputOutputProdutos.Entities {
+    class TotalProduct {
+        public string Product { get; set; }
+        public double TotalPrice { get; set; }
+
+        public TotalProduct(string product,double totalPrice) {
+            Product = product;
+            TotalPrice = totalPrice;
+        }
+    }
+}
---
+++ InputOutputProdutos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using InputOutputProdutos.Entities;
+using System.Globalization;
+
+namespace InputOutputProdutos {
+    class Program {
+        static void Main(string[] args) {
+
+            string SourcePath = @"C:\Temp\arquivo.csv";
+
+            List<TotalProduct> list = new List<TotalProduct>();
+            try {
+                using (StreamReader sr = File.OpenText(SourcePath)) {
+                    while (!sr.EndOfStream) {
+                        string [] line = sr.ReadLine().Split(',');
+                        string NameProduct = line[0];
+                        double Price = double.Parse(line[1],CultureInfo.InvariantCulture);
+                        int Quant = int.Parse(line[2]);
+                        list.Add(new TotalProduct(NameProduct, (Price * Quant)));
+                    }
+                }
+                Directory.CreateDirectory(@"C:\Temp\out");
+                string TargetPath = @"C:\Temp\out\summary.csv";
+                using (StreamWriter sw = File.AppendText(TargetPath)) {
+                    foreach(TotalProduct obj in list) {
+                        sw.WriteLine(obj.Product + "," + obj.TotalPrice.ToString("F2",CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error Ocurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
